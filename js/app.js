1f2cd2305d610eeac857714ec37746daf8e04754
Esthefany Humpiri Vargas <tefavv22@gmail.com>
2017-11-14 08:29:12 -0500

    añade-evento

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var tweetText = document.getElementById('writte-tweet').value;
+
 function tweet() {
+  var tweetText = document.getElementById('writte-tweet').value;
   document.getElementById('new-box').innerHTML = tweetText;
 }
