7129b96ab820f1015ec3148708f5ba8d313f638a
Esthefany Humpiri Vargas <tefavv22@gmail.com>
2017-11-19 04:30:57 -0500

    añade-header

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-
-function tweet() {
-  var tweetText = document.getElementById('writte-tweet').value;
-  document.getElementById('new-box').innerHTML = tweetText;
-}
+window.addEventListener('load', function(event) {
+  var button = document.getElementById('btn');
+  button.addEventListener('click', function(e){
+    var tweetText = document.getElementById('writte-tweet').value;
+    var button = document.getElementById('btn');
+    var tweetBox = document.createElement('div');
+    var replaceText = document.createTextNode(tweetText);
+    tweetBox.appendChild(replaceText);
+    document.body.appendChild(tweetBox);
+    tweetBox.classList.add("tweet-box");
+  });
+});
