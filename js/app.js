f425856b3e87c33eb427462ae8d574130e4282b5
Esthefany Humpiri Vargas <tefavv22@gmail.com>
2017-11-20 10:33:46 -0500

    añade fecha,hora

--- js/app.js
+++ js/app.js
@@ -4,19 +4,11 @@
   var textValue = textArea.value;
   var countCharacters = textValue.length;
   var charactersAllowed = 140;
-  button.addEventListener('click', function(e){
-    var tweetText = document.getElementById('writte-tweet').value;
-    var tweetBox = document.createElement('div');
-    var replaceText = document.createTextNode(tweetText);
-    tweetBox.appendChild(replaceText);
-    textArea.value = "";
-    document.body.appendChild(tweetBox);
-    tweetBox.classList.add("tweet-box");
-  });
+
+  // Se añade un evento al textArea
 
   textArea.addEventListener('keydown', function(e) {
     countBox.value = charactersAllowed - textArea.value.length;
-
     if(countBox.value >= 120) {
      countBox.style.color = 'rgb(14, 194, 75)';
      countBox.style.fontWeight = 'bold';
@@ -24,5 +16,27 @@
      countBox.style.color = 'rgb(233, 26, 26)';
      countBox.style.fontWeight = 'bold';
    }
+  });
+  /*textArea.addEventListener('click', function(e) {
+    if (textArea.textLength === 0) {
+      button.disabled = true;
+      button.style.background = "grey";
+    } else {
+      button.disabled = false;
+    }
+  });*/
+  button.addEventListener('click', function(e){
+    var tweetText = document.getElementById('writte-tweet').value;
+    var tweetBox = document.createElement('div');
+    var replaceText = document.createTextNode(tweetText);
+    var setDate = moment().format('MMMM Do YYYY, h:mm:ss a')
+    var date = document.createElement('p')
+    var dateText = date.textContent = setDate;
+    tweetBox.appendChild(replaceText);
+    tweetBox.appendChild(date);
+    textArea.value = "";
+    document.body.appendChild(tweetBox);
+    tweetBox.classList.add("tweet-box");
+    date.classList.add("date");
 
   });
